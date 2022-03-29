e6195d61c73fc5dffd80326ec793c8c98c18bddd
mfkfawas <mfkfawas@gmail.com>
2022-03-29 15:31:26 +0530

    header & footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,20 @@
+import { Container } from 'react-bootstrap';
+
+import Footer from './layout/Footer';
+import Header from './layout/Header';
+
 function App() {
-  return <>WELCOME TO MY APP</>;
+  return (
+    <>
+      <Header />
+      <Container>
+        <main className='py-3'>
+          <h1>WELCOME TO MY APP</h1>
+        </main>
+      </Container>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
